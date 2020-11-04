2c8cd11c117905121b706ca92439887334e150c8
Dreamy-TZK <dreamytzk@outlook.com>
2020-11-04 16:46:01 +0800

    :sparkles: 登录功能开发完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 配置根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
