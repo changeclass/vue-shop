c9eae89ce111584b575d0e547358145a35d1b73b
XiaoKang <dreamytzk@outlook.com>
2020-11-07 11:55:12 +0800

    ✨ 增加权限管理相关功能

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -74,6 +74,7 @@
                 type="warning"
                 icon="el-icon-setting"
                 size="mini"
+                @click="setRole(scopeEdit.row)"
               ></el-button>
             </el-tooltip>
           </template>
@@ -152,6 +153,36 @@
         <el-button type="primary" @click="editUserInfo">确 定</el-button>
       </span>
     </el-dialog>
+    <!-- 修改角色的对话框 -->
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close="setRoleDialogClosed"
+    >
+      <div>
+        <p>当前的用户：{{ userInfo.username }}</p>
+        <p>当前的角色：{{ userInfo.role_name }}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectedRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in rolesList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </p>
+
+        <p></p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -188,6 +219,8 @@ export default {
       addDialogVisible: false,
       // 控制修改用户的对话框
       editDialogVisible: false,
+      // 控制角色对话框的显示与因此
+      setRoleDialogVisible: false,
       // 添加用户的表单
       addForm: {
         username: '',
@@ -222,7 +255,14 @@ export default {
         mobile: [
           { required: true, message: '请输入手机号', trigger: 'blur' },
           { validator: checkMobile, trigger: 'blur' }]
-      }
+      },
+      // 需要被分配权限的用户信息
+      userInfo: {},
+      // 所有角色的数据列表
+      rolesList: [],
+      // 已经选中的ID值
+      selectedRoleId: ''
+
     }
   },
   created () {
@@ -329,6 +369,38 @@ export default {
         this.$message.success('删除了该用户')
         this.getUserList()
       }
+    },
+    // 展示分配角色的对话框
+    async setRole (userinfo) {
+      // 将角色信息保存到数据
+      this.userInfo = userinfo
+      // 展示对话框前，获取角色的列表
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表')
+      }
+      this.rolesList = res.data
+      // 显示对话框
+      this.setRoleDialogVisible = true
+    },
+    // 点击按钮分配角色
+    async saveRoleInfo () {
+      if (!this.selectedRoleId) {
+        return this.$message.error('请选择要分配的角色')
+      }
+
+      const { data: res } = await this.$http.put(`users/${this.userInfo.id}/role`, { rid: this.selectedRoleId })
+      if (res.meta.status !== 200) {
+        return this.$message.error('更新失败')
+      }
+      this.$message.success('更新角色列表成功')
+      this.getUserList()
+      this.setRoleDialogVisible = false
+    },
+    // 监听关闭对话框事件
+    setRoleDialogClosed () {
+      this.selectedRoleId = ''
+      this.userinfo = ' '
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Home from '../components/Home.vue'
 
 import Welcome from '../components/Welcome.vue'
 import User from '../components/user/User.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -20,7 +22,9 @@ const routes = [
     redirect: '/welcome',
     children: [
       { path: '/welcome', component: Welcome },
-      { path: '/users', component: User }
+      { path: '/users', component: User },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles }
     ]
   }
 ]
